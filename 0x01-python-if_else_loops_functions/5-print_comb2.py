d136c5c5df1bcfa3e3663a6c64cdc1b2d1bf051a
JessicaChideraOgu <ogujessica3@gmail.com>
2023-08-11 23:04:22 +0100

    Corrected task five file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for a in range(100):
-    print(f"{a:02d}", end=", " if a < 99 else "\n",)
+    if a < 100:
+        print(f"{a:02d}".format(a), end=", ")
+    else:
+        print("\n")
