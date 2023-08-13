a453c4a0d60af17b99b5bcc819c06d2cc09b64a5
JessicaChideraOgu <ogujessica3@gmail.com>
2023-08-13 17:25:29 +0100

    Added task zero files

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+The arguments:
+a: first integer
+b: second integer
+
+Returns:
+The return value will be: a + b
+    """
+
+
+    return (a + b)
